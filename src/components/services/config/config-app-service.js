d76927a95ada03f68e7831eaacb7ec67f0e91afa
Lukas Mayerhofer <lmayerhofer.mmt-m2012@fh-salzburg.at>
2015-01-27 13:18:49 +0100

    Removed duplicate license header.

--- src/components/services/config/config-app-service.js
+++ src/components/services/config/config-app-service.js
@@ -5,13 +5,6 @@
  *
  */
 
- /* Copyright (C) 2015 guh
-  *
-  * This software may be modified and distributed under the terms
-  * of the MIT license.  See the LICENSE file for details.
-  *
-  */
-
 (function(){
   "use strict";
 
