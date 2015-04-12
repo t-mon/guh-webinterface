6d4f1dc93c257471ab85cdbccab6973a386ee2fc
Lukas Mayerhofer <info@lukasmayerhofer.at>
2015-04-12 20:12:17 +0200

    Changed device master view and controller to work with added data-store models.

--- src/components/services/models/device-model.js
+++ src/components/services/models/device-model.js
@@ -1,18 +1,18 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *                                                                                     *
  * Copyright (c) 2015 guh                                                              *
  *                                                                                     *
  * Permission is hereby granted, free of charge, to any person obtaining a copy        *
  * of this software and associated documentation files (the "Software"), to deal       *
  * in the Software without restriction, including without limitation the rights        *
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell           * 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell           *
  * copies of the Software, and to permit persons to whom the Software is               *
  * furnished to do so, subject to the following conditions:                            *
  *                                                                                     *
  * The above copyright notice and this permission notice shall be included in all      *
  * copies or substantial portions of the Software.                                     *
  *                                                                                     *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR          * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR          *
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,            *
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE         *
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER              *
@@ -20,67 +20,52 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE       *
  * SOFTWARE.                                                                           *
  *                                                                                     *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 (function(){
   "use strict";
 
   angular
-    .module('guh.devices')
-    .controller('DevicesMasterController', DevicesMasterController);
+    .module('guh.components.models')
+    .factory('DSDevice', DSDeviceFactory)
+    .run(function(DSDevice) {});
 
-  DevicesMasterController.$inject = ['$log', 'DSDevice', 'DSDeviceClass'];
+  DSDeviceFactory.$inject = ['$log', 'DS', 'DSHttpAdapter'];
 
-  function DevicesMasterController($log, DSDevice, DSDeviceClass) {
-
-    /*
-     * Public variables
-     */
-    var vm = this;
-    vm.configured = [];
+  function DSDeviceFactory($log, DS, DSHttpAdapter) {
+    
+    var staticMethods = {};
 
     /*
-     * Private method: _init()
+     * DataStore configuration
      */
-    function _init() {
-      _findAllDevices()
-        .then(_findDeviceClass)
-        .then(function(devices) {
-          vm.configured = devices;
-        })
-        .catch(function(error) {
-          $log.error(error);
-        });
-    }
+    var DSDevice = DS.defineResource({
 
-    /*
-     * Private method: _findAllDevices()
-     */
-    function _findAllDevices() {
-      return DSDevice
-        .findAll();
-    }
+      // API configuration
+      endpoint: 'devices',
+      suffix: '.json',
 
-    /*
-     * Private method: _findDeviceClass(devices)
-     */
-    function _findDeviceClass(devices) {
-      return angular.forEach(devices, function(device) {
-        return DSDevice
-          .loadRelations(device, ['deviceClass'])
-          .then(_findVendor);
-      });
-    }
+      // Model configuration
+      idAttribute: 'id',
+      name: 'device',
+      relations: {
+        belongsTo: {
+          deviceClass: {
+            localField: 'deviceClass',
+            localKey: 'deviceClassId'
+          }
+        }
+      },
 
-    /*
-     * Private method: _findVendor(device)
-     */
-    function _findVendor(device) {
-      return DSDeviceClass
-        .loadRelations(device.deviceClass, ['vendor']);
-    }
+      // Computed properties
+      computed: {},
 
-    _init();
+      // Instance methods
+      methods: {}
+
+    });
+
+    return DSDevice;
 
   }
 
--- src/components/services/models/state-model.js
+++ src/components/services/models/state-model.js
@@ -1,18 +1,18 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *                                                                                     *
  * Copyright (c) 2015 guh                                                              *
  *                                                                                     *
  * Permission is hereby granted, free of charge, to any person obtaining a copy        *
  * of this software and associated documentation files (the "Software"), to deal       *
  * in the Software without restriction, including without limitation the rights        *
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell           * 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell           *
  * copies of the Software, and to permit persons to whom the Software is               *
  * furnished to do so, subject to the following conditions:                            *
  *                                                                                     *
  * The above copyright notice and this permission notice shall be included in all      *
  * copies or substantial portions of the Software.                                     *
  *                                                                                     *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR          * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR          *
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,            *
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE         *
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER              *
@@ -20,67 +20,53 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE       *
  * SOFTWARE.                                                                           *
  *                                                                                     *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 (function(){
   "use strict";
 
   angular
-    .module('guh.devices')
-    .controller('DevicesMasterController', DevicesMasterController);
+    .module('guh.components.models')
+    .factory('DSState', DSStateFactory)
+    .run(function(DSState) {});
 
-  DevicesMasterController.$inject = ['$log', 'DSDevice', 'DSDeviceClass'];
+  DSStateFactory.$inject = ['$log', 'DS'];
 
-  function DevicesMasterController($log, DSDevice, DSDeviceClass) {
+  function DSStateFactory($log, DS) {
 
-    /*
-     * Public variables
-     */
-    var vm = this;
-    vm.configured = [];
+    var staticMethods = {};
 
     /*
-     * Private method: _init()
+     * DataStore configuration
      */
-    function _init() {
-      _findAllDevices()
-        .then(_findDeviceClass)
-        .then(function(devices) {
-          vm.configured = devices;
-        })
-        .catch(function(error) {
-          $log.error(error);
-        });
-    }
+    var DSState = DS.defineResource({
 
-    /*
-     * Private method: _findAllDevices()
-     */
-    function _findAllDevices() {
-      return DSDevice
-        .findAll();
-    }
+      // API configuration
+      endpoint: 'states',
+      suffix: '.json',
 
-    /*
-     * Private method: _findDeviceClass(devices)
-     */
-    function _findDeviceClass(devices) {
-      return angular.forEach(devices, function(device) {
-        return DSDevice
-          .loadRelations(device, ['deviceClass'])
-          .then(_findVendor);
-      });
-    }
+      // Model configuration
+      idAttribute: 'stateTypeId',
+      name: 'state',
+      relations: {
+        belongsTo: {
+          device: {
+            localField: 'device',
+            localKey: 'deviceId',
+            parent: true
+          }
+        }
+      },
 
-    /*
-     * Private method: _findVendor(device)
-     */
-    function _findVendor(device) {
-      return DSDeviceClass
-        .loadRelations(device.deviceClass, ['vendor']);
-    }
+      // Computed properties
+      computed: {},
 
-    _init();
+      // Instance methods
+      methods: {}
+
+    });
+
+    return DSState;
 
   }
 
